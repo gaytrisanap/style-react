1e422b864f5a2e4d834118445ce70cca13cc2cb5
gaytrisanap <sanapgaytri@gmail.com>
2024-09-01 16:07:42 +0900

    Style in react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import greting, { GoodEvening, GoodMoring } from './greting';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <div>
+    <h1 style={{
+        textAlign:"center",
+        color:"black",
+    }}>ALL GRETTING</h1>
+ <greting name="Gayatri"  />
+ <GoodMoring name="Aditi" />
+ <GoodEvening  name="Ankita"/>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ </div>
+)
